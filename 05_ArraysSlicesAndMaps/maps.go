954a78aa33a15f469b206ed05b8b8a182f53fa5f
Kazuki Maehara <treize.citron13@gmail.com>
2022-05-28 16:14:44 +0900

    added an exampe

---
+++ 05_ArraysSlicesAndMaps/maps.go
@@ -0,0 +1,36 @@
+package main
+
+import "fmt"
+
+func main() {
+	elements := map[string]map[string]string{
+		"H": map[string]string{
+			"name":  "Hydrogen",
+			"state": "gas",
+		},
+		"He": map[string]string{
+			"name":  "Helium",
+			"state": "gas",
+		},
+		"Li": map[string]string{
+			"name":  "Lithium",
+			"state": "solid",
+		},
+		"Be": map[string]string{
+			"name":  "Beryllium",
+			"state": "solid",
+		},
+		"B": map[string]string{
+			"name":  "Boron",
+			"state": "solid",
+		},
+		"C": map[string]string{
+			"name":  "Carbon",
+			"state": "solid",
+		},
+	}
+
+	if el, ok := elements["Li"]; ok {
+		fmt.Println(el["name"], el["state"])
+	}
+}
