3c28ef9bdaa555fb7c52047356ab925b58d018c5
Kazuki Maehara <treize.citron13@gmail.com>
2022-12-08 20:41:08 +0900

    added an example

---
+++ 07_StructsAndInterfaces/structs.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+type Circle struct {
+	x, y, r float64
+}
+
+type Rectangle struct {
+	x1, y1, x2, y2 float64
+}
+
+func (r *Rectangle) area() float64 {
+	l := distance(r.x1, r.y1, r.x1, r.y2)
+	w := distance(r.x1, r.y1, r.x2, r.y1)
+	return l * w
+}
+
+func distance(x1, y1, x2, y2 float64) float64 {
+	a := x2 - x1
+	b := y2 - y1
+	return math.Sqrt(a*a + b*b)
+}
+
+func (c *Circle) area() float64 {
+	return math.Pi * c.r * c.r
+}
+
+func main() {
+	c := Circle{x: 0, y: 0, r: 1}
+	fmt.Println(c.area())
+
+	r := Rectangle{x1: 0, y1: 0, x2: 3, y2: 3}
+	fmt.Println(r.area())
+}
